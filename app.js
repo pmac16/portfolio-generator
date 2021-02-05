e6a0fdd59231a6661430d6b953f191bd09cf33ea
Priya Macpherson <priyamacpherson@Priyas-MacBook-Pro.local>
2021-02-04 17:38:48 -0800

    added when for the about section

--- app.js
+++ app.js
@@ -40,9 +40,15 @@ const promptUser = () => {
       type: 'input',
       name: 'about',
       message: 'Provide some information about yourself:',
-      when: ({ confirmAbout }) => confirmAbout
+      when: ({ confirmAbout }) => {
+          if(confirmAbout) {
+              return true;
+          } else {
+              return false;
+          }
+     }
     }
-  ]);
+    ])
 };
 
 const promptProject = portfolioData => {
